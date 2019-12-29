cdedc2ee4dafc5d5c383265537d9441f37bf4bd3
kalliovi <55873258+kalliovi@users.noreply.github.com>
2019-12-29 22:47:46 +0200

    Create sequential.c

---
+++ sequential.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Ising model evolution
+/*!
+  \param G      Spins on the square lattice             [n-by-n]
+  \param w      Weight matrix                           [5-by-5]
+  \param k      Number of iterations                    [scalar]
+  \param n      Number of lattice points per dim        [scalar]
+  NOTE: Both matrices G and w are stored in row-major format.
+/*
+void ising( int *G, double *w, int k, int n){
+}
+
+//Matrix size 517x517 
+//Weights array
+    double weights[] = {0.004, 0.016, 0.026, 0.016, 0.004,
+    		0.016, 0.071, 0.117, 0.071, 0.016,
+			0.026, 0.117, 0, 0.117, 0.026,
+			0.016, 0.071, 0.117, 0.071, 0.016,
+			0.004, 0.016, 0.026, 0.016, 0.004};
